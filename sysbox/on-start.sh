c4e4a9996c7d845b07dfd9c330de54a984980ea0
Ahmad Sameh <ahmadsameh99@gmail.com>
2026-04-06 00:00:20 +0200

    fix: update on-start.sh

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -51,8 +51,8 @@ fi
 # Pull solang image with retries
 echo "Pulling solang image..."
 pull_attempt=0
-# until docker pull ghcr.io/hyperledger-solang/solang@sha256:8a9527c89f01f72ad88e6c13c9f099bcabc040ef24856cc616d56290fde98d3c; 
-untill docker pull ghcr.io/hyperledger-solang/solang:latest; 
+# until docker pull ghcr.io/hyperledger-solang/solang@sha256:8a9527c89f01f72ad88e6c13c9f099bcabc040ef24856cc616d56290fde98d3c;
+until docker pull ghcr.io/hyperledger-solang/solang:latest;
 do
     pull_attempt=$((pull_attempt + 1))
     if [ $pull_attempt -ge 3 ]; then
